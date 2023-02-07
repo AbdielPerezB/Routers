6ee8e46231de9d77f18133aedeb60340dfbebd0f
Abdiel <abdiel2012p@gmail.com>
2023-02-07 15:44:33 -0600

    reouter3 (animals) listo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importamos nuestro framwork
 from fastapi import FastAPI
 #Importamos nuestros 10 routers
-from allRouters import router1, router2
+from allRouters import router1, router2, router3
 
 #Levantamos nuestra instancia de fastAPI
 app = FastAPI()
@@ -9,6 +9,7 @@ app = FastAPI()
 #Incluimos los 10 routers
 app.include_router(router1.routerAutos)
 app.include_router(router2.routerUsuario)
+app.include_router(router3.routerAnimals)
 
 
 #Hcaemos un path del main (opcional)
