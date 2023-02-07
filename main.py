863000f531496efdc144cbe6cac07f8a9f72ed52
Abdiel <abdiel2012p@gmail.com>
2023-02-07 15:33:23 -0600

    router2 (users) listo

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #Importamos nuestro framwork
 from fastapi import FastAPI
 #Importamos nuestros 10 routers
-from allRouters import router1
+from allRouters import router1, router2
 
 #Levantamos nuestra instancia de fastAPI
 app = FastAPI()
 
 #Incluimos los 10 routers
 app.include_router(router1.routerAutos)
+app.include_router(router2.routerUsuario)
 
 
 #Hcaemos un path del main (opcional)
